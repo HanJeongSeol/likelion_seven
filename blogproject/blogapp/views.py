56a05a57c6d9a2c931b06bd9e424ae42b6e8ac7e
hanjeong <hanjeong94@naver.com>
2019-07-09 17:00:14 +0900

    기본 틀 생성

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogModel
 
-# Create your views here.
+def home(request):
+    blogs = BlogModel.objects           # 쿼리셋 가져오기
+    return render(request,'home.html',{'blogs' : blogs})
+
+def detail(request, blog_id):
+    details =get_object_or_404(BlogModel,pk=blog_id)
+    return render(request,'detail.html',{'details':details})
